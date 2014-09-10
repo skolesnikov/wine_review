cd935768972f6c11197ac99481a019c2ae325307
skolesnikov <skolesnikov@gmail.com>
2014-09-10 18:00:45 -0400

    homework for Class 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 WineReview::Application.routes.draw do
-  get "/wines" => "wines#index", as: 'wines'
-  get "/wines/:id" => "wines#show", as: 'wine'
+  resources :wines
+  #get "/wines" => "wines#index", as: 'wines'
+  #get "/wines/new" => "wines#new", as: 'new_wine'
+  #get "/wines/:id" => "wines#show", as: 'wine'
+  #post "/wines" => "wines#create"
+  #get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+  #patch "/wines/:id" => "wines#update"
+  #delete "/wines/:id" => "wines#destroy"
 end
